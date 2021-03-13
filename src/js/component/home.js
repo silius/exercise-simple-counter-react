05fbbda5aed1625ed2db29ed7409d3c9a3a7303e
Adrian Acosta <aacrealms@hotmail.com>
2021-03-13 04:24:40 +0000

    Recovers from crappy bolierplate snafu

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,11 +2,7 @@ import React from "react";
 
 //create your first component
 export function Home() {
-
-    ")(''$di;gits}{}``digits		"--timer-seconds": zero
-	}; urnret        
-2    };
-      {} =>digitsop = "'dsDi'"gitprint constn1cloTime()}'`;
+	let zero = "'0'";
 	const segundos = {
 		"--timer-seconds": zero
 	};
@@ -18,20 +14,20 @@ export function Home() {
 				</p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer n5" style={segundos}></p>
+				<p className="clock-seconds clock-timer" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer n4" style={segundos}></p>
+				<p className="clock-seconds clock-timer" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer n3" style={segundos}></p>
+				<p className="clock-seconds clock-timer" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer n2" style={segundos			</div>
-15""()printDigitspri			<div className="clock-col">
-				<p className="clock-seconds clock-timer n1" style={segundos}></p>
-	
-
-v>
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+		</div>
 	);
 }
