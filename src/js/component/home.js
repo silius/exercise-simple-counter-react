904dcd3ddda95c9a131112ebc6e7556e62e08eb7
Adrian Acosta <aacrealms@hotmail.com>
2021-03-13 02:59:20 +0000

    Adds format

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,33 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	let zero = "'0'";
+	const segundos = {
+		"--timer-seconds": zero
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="clock-container">
+			<div className="clock-col">
+				<p className="icons">
+					<i className="far fa-clock"></i>
+				</p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
+			<div className="clock-col">
+				<p className="clock-seconds clock-timer" style={segundos}></p>
+			</div>
 		</div>
 	);
 }
