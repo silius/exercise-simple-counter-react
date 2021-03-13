bcf4999becb4dc9394603483f068fbff8f886e4e
Adrian Acosta <aacrealms@hotmail.com>
2021-03-13 04:16:50 +0000

    Restores base template

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 //create your first component
 export function Home() {
-	let zero = "'0'";
+
+    ")(''$di;gits}{}``digits		"--timer-seconds": zero
+	}; urnret        
+2    };
+      {} =>digitsop = "'dsDi'"gitprint constn1cloTime()}'`;
 	const segundos = {
 		"--timer-seconds": zero
 	};
@@ -14,20 +18,20 @@ export function Home() {
 				</p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer" style={segundos}></p>
+				<p className="clock-seconds clock-timer n5" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer" style={segundos}></p>
+				<p className="clock-seconds clock-timer n4" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer" style={segundos}></p>
+				<p className="clock-seconds clock-timer n3" style={segundos}></p>
 			</div>
 			<div className="clock-col">
-				<p className="clock-seconds clock-timer" style={segundos}></p>
-			</div>
-			<div className="clock-col">
-				<p className="clock-seconds clock-timer" style={segundos}></p>
-			</div>
-		</div>
+				<p className="clock-seconds clock-timer n2" style={segundos			</div>
+15""()printDigitspri			<div className="clock-col">
+				<p className="clock-seconds clock-timer n1" style={segundos}></p>
+	
+
+v>
 	);
 }
